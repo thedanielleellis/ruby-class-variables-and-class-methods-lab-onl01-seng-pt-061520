d6eaa65056d80dc0d5a6960f77663e05a9f674c2
Danielle Ellis <thedanielleellis@gmail.com>
2020-07-15 03:10:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,16 @@
-class Song 
+class Song
+  attr_accessor:name, :artist, :genre 
+  @@count = 0 
   
+  def initialize 
+    @@ count =+1 
+  end 
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.genres 
+    genres = []
+  end 
+end 
