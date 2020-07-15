d2e175afbe69768d9b5a1da628ea435e7375439d
Danielle Ellis <thedanielleellis@gmail.com>
2020-07-15 03:24:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   @@count = 0 
   
   def initialize 
-    @@ count =+1 
+    @@count =+1 
   end 
   
   def self.count 
@@ -11,6 +11,17 @@ class Song
   end 
   
   def self.genres 
-    genres = []
+    @@genres = []
+  end 
+  
+  def self.artists
+    @@artist = []
+    
+  def self.genre_count 
+    self
+  end 
+  
+  def self.artist_count
+  end 
   end 
 end 
